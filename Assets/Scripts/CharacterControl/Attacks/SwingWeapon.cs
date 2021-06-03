e813b01a3752b0334e0191c3cd34ca851dcbec25
arrionDay <67357597+arrionDay@users.noreply.github.com>
2021-06-02 22:41:21 -0700

    Added charge attack to the combat

--- Assets/Scripts/CharacterControl/Attacks/SwingWeapon.cs
+++ Assets/Scripts/CharacterControl/Attacks/SwingWeapon.cs
@@ -4,43 +4,66 @@ using UnityEngine;
 
 public class SwingWeapon : MonoBehaviour
 {
-  public GameObject hitbox;
-  private Renderer hitboxRenderer;
-  public static GameObject weapon;
+  //hit box related things, will not be as needed when animations are put in.
+  public GameObject hitbox; //to manipulate to swing for visual testing
+  private Renderer hitboxRenderer; //to change color for visual testing
+  public static GameObject weapon; //this is how we will get reference to the hitbox
+  private Vector3 StartRot; //starting rotation angle for hitbox
   private Rigidbody rb;
+
+
+
+  //relating towards momentum from smacking
   public int clickForce = 500;
   private Plane plane = new Plane(Vector3.up, Vector3.zero);
-  //timers needed to perform a swing
+  //celleration for when smacking and momentum
   public float accel;
   public float decel;
-  public float startUpTime;
+
+  //timers needed to perform a swing
+  public float startUpTime; //startup time to perform swing
   private float startUpTimer;
-  public float cooldownTime = 0.1f;
-  public static float cooldownTimer = 0f;
-  private float swingTime = 0.05f;
+
+  private float swingTime = 0.05f; //how long the swing lasts
   private float swingTimer;
-  private int swingState = 0;
+
+  public float cooldownTime; //cooldown between each swing
+  public static float cooldownTimer = 0f;
+
+
+  private int swingState = 0; //swing state to determine which part of the swing we are at
+  public bool bufferAttack; //in order to be able to buffer commands before cooldown is up
+
+
 
   //to reset position to what it was before
   public static Quaternion savedRotationSW;
   public static Vector3 savedPositionSW;
 
-  //steps in the combo
+  //steps in the combo and timers for combos breaking
   public int comboStep = 0;
   public float comboTimer;
-  private Vector3 StartRot; //starting rotation angle for hitbox
+
+
+  //related to the charge attack and differentiating between tap and hold
+  public float holdTimer = 0f;
+  public float tapThreshold;
+  public float chargePercent;
+  private float maxCharge = 1f;
+  private bool chargeAttack = false;
+
     // Start is called before the first frame update
     void Start()
     {
       hitboxRenderer = hitbox.GetComponent<Renderer>();
       rb = GetComponent<Rigidbody>();
-      //finding the weapon to manipulate
+      //finding the weapon to manipulate the rotation
       foreach (Transform child in transform)
       {
           if(child.tag == "Weapon")
           {
               weapon = child.gameObject;
-              savedRotationSW = weapon.transform.rotation;
+              savedRotationSW = weapon.transform.rotation; //saving the original position of the hitbox to revert to after hits are done
               savedPositionSW = weapon.transform.localPosition;
           }
       }
@@ -49,70 +72,154 @@ public class SwingWeapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      //cant dash and swing
+      if (PlayerMovement.dashState != 1) {
 
-      switch(comboStep) {
-        case 0:
-          StartRot = new Vector3(90f, 90f, 0f);
-          performSwing(StartRot, -3600, 1, 0.05f);
-          break;
+        //Steps along the combo chain negative means recovery time
+        //Case 0: first swing to start combo
+        //Case 1: second swing on the combo
+        //case 2: last hit
+        //case -1 to -3, different recovery phases dependent on which part of the combo you broke out
+        switch(comboStep) {
+          case 0:
+            if (Input.GetMouseButton(0)) {
+              holdTimer += Time.deltaTime;
+              if (holdTimer >= tapThreshold) {
+                PlayerMovement.isAction = true;
+                rb.velocity = new Vector3(0, 0, 0);
+                hitboxRenderer.material.SetColor("_Color", Color.magenta);
+                if (chargePercent < maxCharge) {
+                  chargePercent += Time.deltaTime;
+                }
+              }
+            }
+            if (Input.GetMouseButtonUp(0)) {
+              if (holdTimer >= tapThreshold) { //do charge attack if tapthreshold is met
+                chargeAttack = true;
+              }
+              bufferAttack = true;
+              holdTimer = 0f;
+            }
 
-        case 1:
-          comboTimer += Time.deltaTime;
-          if (comboTimer > 0.5f) {
-            comboStep = -1;
-            comboTimer = 0f;
+            if (chargeAttack == true) {
+              StartRot = new Vector3(0f, 90f, 0f);
+              performSwing(StartRot, 1800, -4, 0.1f, 0.2f, true);
+            }
+            else {
+              StartRot = new Vector3(90f, 90f, 0f);
+              performSwing(StartRot, -3600, 1, 0.05f, 0.2f, false);
+            }
+            break;
+
+          case 1:
+            if (Input.GetMouseButton(0)) {
+              holdTimer += Time.deltaTime;
+              if (holdTimer >= tapThreshold) {
+                PlayerMovement.isAction = true;
+                rb.velocity = new Vector3(0, 0, 0);
+                hitboxRenderer.material.SetColor("_Color", Color.magenta);
+                if (chargePercent < maxCharge) {
+                  chargePercent += Time.deltaTime;
+                }
+              }
+            }
+            if (Input.GetMouseButtonUp(0)) {
+              if (holdTimer >= tapThreshold) { //do charge attack if tapthreshold is met
+                chargeAttack = true;
+              }
+              bufferAttack = true;
+              holdTimer = 0f;
+            }
+
+            if (chargeAttack == true) {
+              StartRot = new Vector3(0f, 90f, 0f);
+              performSwing(StartRot, 1800, -4, 0.1f, 0.2f, true);
+            }
+            else {
+              StartRot = new Vector3(90f, -90f, 0f);
+              performSwing(StartRot, 3600, 2, 0.05f, 0.2f, false);
+            }
+
+            setComboTimer(0.5f, 0);
+            break;
+
+          case 2:
+            if (Input.GetMouseButton(0)) {
+              holdTimer += Time.deltaTime;
+              if (holdTimer >= tapThreshold) {
+                PlayerMovement.isAction = true;
+                rb.velocity = new Vector3(0, 0, 0);
+                hitboxRenderer.material.SetColor("_Color", Color.magenta);
+                if (chargePercent < maxCharge) {
+                  chargePercent += Time.deltaTime;
+                }
+              }
+            }
+            if (Input.GetMouseButtonUp(0)) {
+              if (holdTimer >= tapThreshold) { //do charge attack if tapthreshold is met
+                chargeAttack = true;
+              }
+              bufferAttack = true;
+              holdTimer = 0f;
+            }
+
+            if (chargeAttack == true) {
+              StartRot = new Vector3(0f, 90f, 0f);
+              performSwing(StartRot, 1800, -4, 0.1f, 0.2f, true);
+            }
+            else {
+              StartRot = new Vector3(45f, 90f, 0f);
+              performSwing(StartRot, -1800, -3, 0.05f, 0.2f, false);
+            }
+            setComboTimer(0.5f, 0);
+            break;
+
+          case -1:
+            if (Input.GetMouseButtonUp(0)) {
+              bufferAttack = true;
+            }
+            setComboTimer(0.3f, 0);
+            break;
+
+          case -2:
+            setComboTimer(0.4f, 0);
+            break;
+
+          case -3:
+            setComboTimer(0.4f, 0);
+            break;
+          case -4:
+            setComboTimer(0.7f, 0);
             break;
           }
-          StartRot = new Vector3(90f, -90f, 0f);
-          performSwing(StartRot, 3600, 2, 0.05f);
-          break;
-
-        case 2:
-          comboTimer += Time.deltaTime;
-          if (comboTimer > 0.5f) {
-            comboStep = -2;
-            comboTimer = 0f;
-            break;
-          }
-          StartRot = new Vector3(45f, 90f, 0f);
-          performSwing(StartRot, -3600, -3, 0.05f);
-          break;
-
-
-        case -1:
-          comboTimer += Time.deltaTime;
-          if (comboTimer > 0.3f) {
-            comboStep = 0;
-          }
-          break;
-
-        case -2:
-          comboTimer += Time.deltaTime;
-          if (comboTimer > 0.4f) {
-            comboStep = 0;
-          }
-          break;
-
-        case -3:
-          comboTimer += Time.deltaTime;
-          if (comboTimer > 0.4f) {
-            comboStep = 0;
-          }
-          break;
       }
+
     }
 
-    void performSwing(Vector3 StartRotation, int RotationSpeed, int NextStep, float activeTime) {
+    //setting a time before moving to the next step in the combo
+    void setComboTimer(float maxTime, int NextStep) {
+      comboTimer += Time.deltaTime;
+        if (comboTimer > maxTime) {
+          comboStep = NextStep;
+          comboTimer = 0f;
+        }
+    }
+
+    //steps along the hitting process
+    //Case 0: idle state check for inputs to start the slash
+    //Case 1: start up animation and while accelerating in direction of the hit
+    //Case 2: Active frames where it is slashing, will check for hitbox here.
+    //Case -1: Recovery time before being able to move again.
+    void performSwing(Vector3 StartRotation, int RotationSpeed, int NextStep, float activeTime, float recoveryTime, bool ifCharge) {
       switch (swingState) {
-        case 0: //Starting state,
-          hitboxRenderer.material.SetColor("_Color", Color.white);
-          var isSlashKeyDown = Input.GetButton("Fire1");
-          if(isSlashKeyDown)
+        case 0: //Starting state
+
+
+          if(bufferAttack) //if slashing or a slash is buffered then perform the action
             {
-              //dashing in the direction of the mouse code\
+              //dashing in the direction of the mouse for some momentum. raycast to a floor, then add force ein that direction
               rb.velocity = new Vector3(0, 0, 0);
               var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
               float enter;
               if (plane.Raycast(ray, out enter))
               {
@@ -123,45 +230,72 @@ public class SwingWeapon : MonoBehaviour
 
               //setting variables for next steps + rotating weapon to attack position
               PlayerMovement.isAction = true;
-              swingState = 1;
               weapon.transform.localRotation = Quaternion.Euler(StartRotation.x, StartRotation.y, StartRotation.z);
-              swingTimer = 0;
+              swingState = 1;
               startUpTimer = 0;
+              bufferAttack = false;
           }
         }
         break;
 
         case 1: //start up
-        rb.velocity = rb.velocity * decel;
         hitboxRenderer.material.SetColor("_Color", Color.red);
+
+        //decelerate the momentum during startup
+        rb.velocity = rb.velocity * decel;
+
+        //timer to switch to active frames
           startUpTimer += Time.deltaTime;
           if (startUpTimer >= startUpTime) {
+            swingTimer = 0;
             swingState = 2;
           }
           break;
+
         case 2: //Active
-          rb.velocity = new Vector3(0f,0f,0f);
           hitboxRenderer.material.SetColor("_Color", Color.green);
-          weapon.transform.RotateAround(this.transform.position, transform.up, Time.deltaTime * RotationSpeed);
+          //stop all momentum at this point
+          rb.velocity = new Vector3(0f,0f,0f);
+
+          //rotate the swing
+          if (ifCharge) {
+            weapon.transform.RotateAround(this.transform.position, transform.right, Time.deltaTime * RotationSpeed);
+          }
+          else {
+            weapon.transform.RotateAround(this.transform.position, transform.up, Time.deltaTime * RotationSpeed);
+          }
+          //timer before switching to recovery stage
           swingTimer += Time.deltaTime;
           if(swingTimer >= activeTime)
           {
               swingTimer = swingTime;
+
               cooldownTimer = 0f;
               swingState = -1;
 
           }
           break;
+
         case -1: //recovery
           hitboxRenderer.material.SetColor("_Color", Color.blue);
+
+          //be able to buffer attack during recovery stage
+      //    if (Input.GetMouseButton(0))
+        //  {
+          //    bufferAttack = true;
+          //}
+
+          //timer to reset to the next combostep and reset the transforms
           cooldownTimer += Time.deltaTime;
-          if (cooldownTimer >= cooldownTime) {
+          if (cooldownTimer >= recoveryTime) {
             weapon.transform.rotation = savedRotationSW;
             weapon.transform.localPosition = savedPositionSW;
             comboStep = NextStep;
-            PlayerMovement.isAction = false;
+            PlayerMovement.isAction = false; //let them MOVE AGAIN
+            chargeAttack = false;
             comboTimer = 0f; //in reference to the combo attack system
             swingState = 0;
+            hitboxRenderer.material.SetColor("_Color", Color.white);
           }
           break;
       }
--- Assets/Scripts/CharacterControl/Movement/PlayerMovement.cs
+++ Assets/Scripts/CharacterControl/Movement/PlayerMovement.cs
@@ -5,17 +5,19 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
+  //Relating towards movement and velocity
   Rigidbody rb;
-  float speed = 13f;
+  public float speed = 13f;
 
-  public int dashState;
+  //Dashing related variables, used as a timer for recovery and how long the dash lasts for
+  public static int dashState;
   public float dashTimer;
-  public float maxDash = 1f;
-  public Vector3 savedVelocity;
-  public Quaternion lastRotation;
+  public float maxDash;
 
+  //to prevent running while fighting
   public static bool isAction = false;
 
+  //to look at mouse lol
   private Vector3 lookTarget;
 
   void Start ()
@@ -23,32 +25,43 @@ public class PlayerMovement : MonoBehaviour
       rb = GetComponent<Rigidbody>();
   }
 
+//UPDATE FRAME EVERY FRAME DO INPUT CHECKS
   void Update () {
     float horiz = Input.GetAxisRaw ("Horizontal");
     float vert = Input.GetAxisRaw ("Vertical");
 
-    //to perform dashes
+    //runs a check to determine if player wants to dash and then performs it
     performDash(horiz, vert);
 
-    //You can only move while not dashing
-    //All the movement stuff
-    if (!isAction){
-        if (dashState != 1){
-          lookAtMouse();
-          if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
-            Vector3 fVelocity = new Vector3(horiz, 0f, vert);
-            rb.velocity = fVelocity.normalized * speed;
-        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(fVelocity.normalized), 0.02F);
-        //    lastRotation = transform.rotation;
-          }
-          else {
-        //    transform.rotation = Quaternion.Slerp(transform.rotation, lastRotation, 0.15F);
-            rb.velocity = new Vector3(0,0,0);
-          }
-       }
-   }
+    //checks for directional inputs and makes the player run in that direction
+    performMovement(horiz, vert);
 }
 
+
+  //You can only move while not dashing or isnt fighting isAction checks for fighting
+  //normal running stuff
+  void performMovement(float horiz, float vert) {
+    if (!isAction){
+      if (dashState != 1){
+        lookAtMouse();//always look towards mouse
+
+        //if there is any direction inputs, run in that direction
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
+          Vector3 fVelocity = new Vector3(horiz, 0f, vert);
+          rb.velocity = fVelocity.normalized * speed;
+        }
+
+        else {
+          rb.velocity = new Vector3(0,0,0); //no input = stand still
+        }
+      }
+    }
+  }
+
+//switch case to determine dashing state and whatnot
+//case 0: not dashing. in this phase check for space input, if pressed then face direction of movement and set velocity in that direction
+//case 1: run a timer for how long the dash will last. when the duration is over, set dash state back so that moment is stopped again
+//case -1: cooldown window. set a timer and when it is over, reset back to stage 0
   void performDash(float horiz, float vert){
     switch (dashState)
            {
@@ -57,13 +70,14 @@ public class PlayerMovement : MonoBehaviour
                if(isDashKeyDown)
                  {
                    if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
+
                      SwingWeapon.weapon.transform.rotation = SwingWeapon.savedRotationSW;
                      SwingWeapon.weapon.transform.localPosition = SwingWeapon.savedPositionSW;
 
+                     //get the input data and normalize it to have a direction vector. then simply multiply it with speed. also look in direction of the dash which is just the normalized direction vector.
                      Vector3 fVelocity = new Vector3(horiz, 0f, vert);
                      rb.velocity = fVelocity.normalized * speed * 3;
                      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(fVelocity.normalized), 0.02F);
-                     lastRotation = transform.rotation;
                    }
                    dashState = 1;
                }
@@ -72,16 +86,15 @@ public class PlayerMovement : MonoBehaviour
                dashTimer += Time.deltaTime * 3;
                if(dashTimer >= maxDash)
                {
-                   dashTimer = maxDash;
-                   GetComponent<Rigidbody>().velocity = savedVelocity;
-              //     transform.rotation = Quaternion.LookRotation(savedVelocity);
-                   dashState = -1;
+                   dashTimer = maxDash; //idk why this is done this way LOL but its basically counting backwards for recovery time maybe i should change it for consistency soon :p
+                   dashState = -1; //no longer dashing
                    SwingWeapon.cooldownTimer = 0f;
+                   rb.velocity = new Vector3(0,0,0); //stop after dash ends
                }
                break;
            case -1:
                dashTimer -= Time.deltaTime;
-               if(dashTimer <= 0)
+               if(dashTimer <= 0) //when recovery time is up reset everything so u can dash again :)
                {
                    dashTimer = 0;
                    dashState = 0;
@@ -90,6 +103,7 @@ public class PlayerMovement : MonoBehaviour
            }
   }
 
+  //raycast the mouse position and make the transform look at it. change the angle to ignore x z so that its just pure rotation.
   void lookAtMouse(){
     var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
     RaycastHit hit;
