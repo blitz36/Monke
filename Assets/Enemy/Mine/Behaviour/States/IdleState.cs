550bb1ece1384a17cb16abfaf1020b2ad73528de
crowbardispensingcore <crowbargel@gmail.com>
2021-07-07 15:45:10 -0700

    new working scene

--- Assets/Enemy/Mine/Behaviour/States/IdleState.cs
+++ Assets/Enemy/Mine/Behaviour/States/IdleState.cs
@@ -6,13 +6,26 @@ public class IdleState : State
 {
     [SerializeField] private float alarmRange;
     [SerializeField] private float triggerRange;
-    [SerializeField] private LayerMask PlayerLayer;
-    public ActivateState activateState;
-    public bool isInTriggerRange = false;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private ActivateState activateState;
+    private bool isInTriggerRange = false;
+    private bool isInAlertRange = false;
     
 
-    public override State runCurrentState()
+    public override State runCurrentStateUpdate(StateController controller)
     {
+        if (!isInAlertRange)
+        {
+            controller.lightLerpSpeed = 1f;
+        }
+        else if (isInAlertRange && !isInTriggerRange)
+        {
+            controller.lightLerpSpeed = 10f;
+        }
+
+        flashing(controller);
+
+        //Debug.Log("Now in idle state!");
         if (isInTriggerRange)
         {
             return activateState;
@@ -23,11 +36,30 @@ public class IdleState : State
         }
     }
 
-    private void FixedUpdate()
+    public override void runCurrentStateFixedUpdate(StateController controller)
     {
-        if (!isInTriggerRange)
+        isInAlertRange = Physics.CheckSphere(transform.position, alarmRange, playerLayer);
+        isInTriggerRange = Physics.CheckSphere(transform.position, triggerRange, playerLayer);
+    }
+
+    private void flashing(StateController controller)
+    {
+        if (controller.lightLerpDirection)
         {
-            isInTriggerRange = Physics.CheckSphere(transform.position, triggerRange, PlayerLayer);
+            controller.lightLerpTimer += Time.deltaTime * controller.lightLerpSpeed;
+        }
+        else
+        {
+            controller.lightLerpTimer -= Time.deltaTime * controller.lightLerpSpeed;
+        }
+        controller.statusLight.intensity = Mathf.Lerp(0f, 1f, controller.lightLerpTimer);
+        if (controller.lightLerpTimer <= 0f)
+        {
+            controller.lightLerpDirection = true;
+        }
+        if (controller.lightLerpTimer >= 1f)
+        {
+            controller.lightLerpDirection = false;
         }
     }
 }
