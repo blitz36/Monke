6c8902454f22275dabf9ad650e9102858b0dd26e
arrionDay <67357597+arrionDay@users.noreply.github.com>
2021-07-14 15:59:01 -0700

    title menu change

--- Assets/Scenes/Start Menu/TitleMenu.cs
+++ Assets/Scenes/Start Menu/TitleMenu.cs
@@ -8,10 +8,10 @@ public class TitleMenu : MonoBehaviour
     public GameObject player;
     public void StartGame ()
     {
-        Instantiate(player, new Vector3(0,0,0), Quaternion.identity);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-		Time.timeScale=1f;
-    }
+      Instantiate(player, new Vector3(0,0,0), Quaternion.identity);
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		  Time.timeScale=1f;
+     }
 
 	public void QuitGame ()
 	{
