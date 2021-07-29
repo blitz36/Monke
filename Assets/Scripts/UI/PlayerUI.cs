c66a9c521d46cdbc13e1a8055ac7124e8b425a85
rookssssss <huando87@yahoo.com>
2021-07-29 09:40:39 -0700

    more ui

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -20,7 +20,7 @@ public class PlayerUI : MonoBehaviour
     {
       float fps = 1/Time.unscaledDeltaTime;
       fpsDisplay.text = "" + fps;
-      dashDisplay.text = "Dashes Left:" + PSM.numDashes;
+      dashDisplay.text = "x" + PSM.numDashes;
     //  equipDisplay.text = "Charge Available:" + PSM.canUseEquip;
     }
 }
