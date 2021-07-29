fb53128246b2f8522f2f29076e54bbde6b839df6
rookssssss <huando87@yahoo.com>
2021-07-29 13:27:40 -0700

    uiasdasdasd

---
+++ Assets/uiFollowMouse.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiFollowMouse : MonoBehaviour
+{
+    public RectTransform MovingObject;
+    public Vector3 offset;
+    public RectTransform CanvasObject;
+    public Canvas m_canvas;
+
+    public void Update()
+    {
+        //MoveObject();
+        Vector3 vector = new Vector3(Input.mousePosition.x, InputmousePosition.y, Camera.main.nearClipPlane.ClipPlane);
+        Debug.Log(Camera.main.ScreenToWorldPoint(vector));
+    }
+
+    //public void MoveObject()
+    //{
+        //MovingObject.anchoredPosition = Input.mousePosition;
+        
+        //Vector2 anchoredPos;
+        //RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasObject, Input.mousePosition, m_canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : cam, out anchoredPos);
+        //MovingObject.anchoredPosition = anchoredPos;
+    //}
+}
